8ba6083141308e61f08a0fdf87ec4447b16239e8
squeemishly <squeemishly@gmail.com>
2017-08-30 14:48:25 -0600

    creates meal_foods migration and seed file, adds CASCADE to foods and meals seed files TRUNCATE calls

---
+++ db/seeds/dev/meal-foods.js
@@ -0,0 +1,18 @@
+exports.seed = function(knex, Promise) {
+  return knex.raw('TRUNCATE meal_foods RESTART IDENTITY CASCADE')
+  .then(function() {
+    return Promise.all([
+      knex.raw('INSERT INTO meal_foods (food_id, meal_id) VALUES (?, ?)', [3 , 1]),
+      knex.raw('INSERT INTO meal_foods (food_id, meal_id) VALUES (?, ?)', [5 , 1]),
+      knex.raw('INSERT INTO meal_foods (food_id, meal_id) VALUES (?, ?)', [7 , 1]),
+      knex.raw('INSERT INTO meal_foods (food_id, meal_id) VALUES (?, ?)', [9 , 2]),
+      knex.raw('INSERT INTO meal_foods (food_id, meal_id) VALUES (?, ?)', [2 , 2]),
+      knex.raw('INSERT INTO meal_foods (food_id, meal_id) VALUES (?, ?)', [10 , 2]),
+      knex.raw('INSERT INTO meal_foods (food_id, meal_id) VALUES (?, ?)', [4 , 3]),
+      knex.raw('INSERT INTO meal_foods (food_id, meal_id) VALUES (?, ?)', [7 , 3]),
+      knex.raw('INSERT INTO meal_foods (food_id, meal_id) VALUES (?, ?)', [1 , 3]),
+      knex.raw('INSERT INTO meal_foods (food_id, meal_id) VALUES (?, ?)', [6 , 4]),
+      knex.raw('INSERT INTO meal_foods (food_id, meal_id) VALUES (?, ?)', [11 , 4])
+    ])
+  })
+};
--- db/seeds/dev/meals.js
+++ db/seeds/dev/meals.js
@@ -1,5 +1,5 @@
 exports.seed = function(knex, Promise) {
-  return knex.raw('TRUNCATE meals RESTART IDENTITY')
+  return knex.raw('TRUNCATE meals RESTART IDENTITY CASCADE')
   .then(function() {
     return Promise.all([
       knex.raw('INSERT INTO meals (name, created_at) VALUES (?, ?)', ["Breakfast", new Date]),
