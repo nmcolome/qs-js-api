1867a46b65603c61d95909a8441d7f6784ffce88
squeemishly <squeemishly@gmail.com>
2017-08-30 20:44:12 -0600

    logic moved from server to Food model

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const Food = require('./lib/models/food')
 const app = require('express')()
 const request = require('request')
 
@@ -12,71 +13,23 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.set('port', process.env.PORT || 3000)
 
 app.get('/api/v1/foods', (request, response) => {
-  database.raw(`SELECT id, name, calories FROM foods`)
-  .then(data => {
-    response.json(data.rows)
-  })
+  Food.getAllFoods(response)
 })
 
 app.get('/api/v1/foods/:id', (request, response) => {
-  const { id } = request.params
-  database.raw(`SELECT id, name, calories FROM foods WHERE id=?`, [id])
-  .then(data => {
-    if (data.rows.length < 1) {
-      return response.sendStatus(404)
-    } else {
-      response.json(data.rows)
-    }
-  })
+  Food.getOneFood(request, response)
 })
 
 app.post('/api/v1/foods', (request, response) => {
-  const food = request.body
-  const name = food.food.name
-  const calories = food.food.calories
-  if(name === "" || calories === "") {
-    return response.sendStatus(400)
-  } else {
-    database.raw(
-      'INSERT INTO foods (name, calories, created_at) VALUES (?, ?, ?) RETURNING id, name, calories',
-      [name, calories, new Date]
-    )
-    .then((data) => {
-      response.json(data.rows)
-    })
-  }
+  Food.createFood(request, response)
 })
 
 app.delete('/api/v1/foods/:id', (request, response) => {
-  const { id } = request.params
-  database.raw(`DELETE FROM foods WHERE id = ?`, [id])
-  .then((data) => {
-    if (data.rowCount < 1) {
-      response.sendStatus(404)
-    } else {
-      response.sendStatus(200)
-    }
-  })
+  Food.deleteFood(request, response)
 })
 
 app.put('/api/v1/foods/:id', (request, response) => {
-  const { id } = request.params
-  const updatedFood = request.body.food
-  const name = updatedFood.name
-  const calories = updatedFood.calories
-
-  if(name === "" || calories === "") {
-    response.sendStatus(400)
-  } else {
-    database.raw(`UPDATE foods SET name = ?, calories = ? WHERE id = ? RETURNING id, name, calories`, [name, calories, id])
-    .then(data => {
-      if(data.rowCount < 1) {
-        response.sendStatus(404)
-      } else {
-        response.json(data.rows[0])
-      }
-    })
-  }
+  Food.updateFood(request, response)
 })
 
 app.get('/api/v1/meals', (request, response) => {
